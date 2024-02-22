75b5e1166a50390154750eec3134a1e1cb0ed5a2
AmeyaKI <146046465+AmeyaKI@users.noreply.github.com>
2024-02-22 13:06:55 -0800

    Update Grasstype.java

--- Pokemon Proj/Grasstype.java
+++ Pokemon Proj/Grasstype.java
@@ -2,20 +2,17 @@ import java.util.Random;
 
 public class Grasstype extends Pokemon{
 
-    //constructor that intializes the name, atkpower, hp, and type to grass, sets hp and maxhp to hp
+    //constructor: intializes  name, atkpower, hp, and type to grass, sets hp and maxhp to hp
     public Grasstype(String n,int atk, int h){
       super(n, atk, h);
-      setType("grass");
-      
-    //code goes here 
+      setType("grass");    
     }
     
-    //randomly picks from "absorb","razor leaf", and "solar beam" and returns them.
+    // randomly picks from "absorb","razor leaf", and "solar beam" and returns them.
     public String attackMoveName(){
       String list[] = {"absorbe", "razor leaf", "solar beam"};
       Random rand = new Random();
       int randomInteger = rand.nextInt(3);
       return list[randomInteger];
-      //code goes here
     }
 }
